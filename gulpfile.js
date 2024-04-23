dfe3d6e6053e9727a4b1a13e55c919721a9652f3
Nataliia <natashazubleva4@gmail.com>
2024-04-23 17:39:04 +0100

    add scss

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,26 @@
 const gulp = require('gulp');
+const sass = require('sass');
+const gulpSass = require('gulp-sass');
+const scss = gulpSass(sass);
 
 const BUILD_JS_FOLDER = './dist/js';
+const BUILD_CSS_FOLDER = './dist/styles';
 const SRC_FOLDER = './src/js/*.js';
+const SRC_FOLDER_SCSS = './src/styles/*.scss';
 
-gulp.task('default', () => {
- return gulp.src(SRC_FOLDER)
- .pipe(gulp.dest(BUILD_JS_FOLDER));
-});
+function watcher() {
+    return gulp.watch(SRC_FOLDER, copy);
+}
+
+function copy() {
+    return gulp.src(SRC_FOLDER)
+    .pipe(gulp.dest(BUILD_JS_FOLDER));
+}
+
+function scssTask(){
+    return gulp.src(SRC_FOLDER_SCSS)
+    .pipe(scss())
+    .pipe(gulp.dest(BUILD_CSS_FOLDER));
+}
+
+gulp.task('default', gulp.series(copy, scssTask, watcher));
--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,14 @@
-let a=10;
+let a = 10;
 console.log(a+a);
+
+let b = 10;
+console.log(a+a);
+
+let c = 10;
+console.log(a+b);
+
+
+let f = 10;
+console.log(a+b);
+
+
