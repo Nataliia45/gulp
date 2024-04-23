77f28857e4d8a41ddf9181b2713bdc4de638e23f
Nataliia <natashazubleva4@gmail.com>
2024-04-23 18:02:44 +0100

    add minify

--- src/js/humen.js
+++ src/js/humen.js
@@ -3,3 +3,13 @@ console.log(a+a);
 
 let b = 10;
 console.log(a+a);
+
+
+class Human{
+    constructor(){
+        console.log("lalala")
+    }
+    show(){
+        document.body.textContent = "Hello";
+    }
+}
